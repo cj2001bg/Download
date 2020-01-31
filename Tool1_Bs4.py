2b6848d1671eccaa0d05ece98741fe9e754c7ad2
Jane.Chen <56249057+cj2001bg@users.noreply.github.com>
2020-01-31 09:47:49 +0800

    Create Tool1_Bs4.py

---
+++ Tool1_Bs4.py
@@ -0,0 +1,29 @@
+#用BS4解析数据，提取数据
+
+import requests
+from bs4 import BeautifulSoup
+# URL为目标网址
+URL = 'https://localprod.pandateacher.com/python-manuscript/crawler-html/spider-men5.0.html'
+res = requests.get(URL)
+
+if res.status_code == 200:
+    soup = BeautifulSoup(res.text,'html.parser')
+# 缩小搜索范围：soup.find，soup.find_all,soup.标签名,...
+# 精确提取信息：tag.text文本内容；tag['属性名']，正则，字符串处理等方式
+# 存储到文件：
+# 1.存成列表 
+# 2.with open('文件名.后缀','打开方式') as file:
+# 3. file.write(列表) 
+# 4. print('写入完毕')
+
+    items = soup.find_all(class_='books')
+    for item in items:
+        kind = item.h2
+        title = item.find(class_='title')
+        brief = item.find(class_='info')
+        print(kind.text)
+        print(title.text)
+        print(title['href'])
+        print(brief.text)
+    
+
